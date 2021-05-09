9d77be7f9c40aefc691405b3bb39b88812e298a3
Jan Marasigan <1756489@johnabbottcollege.net>
2021-05-09 13:41:04 -0400

    initial classes

---
+++ WorkPunch/WorkPunch/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPunch
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private int age;
+        private List<Job> jobs;
+        private List<Work_Week> workWeeks;
+
+        public Employee()
+        {
+
+        }
+
+    }
+}
