5f79b14c5dbbb28687c7f9457fb572bd30c50f09
Patrick <1170313@johnabbottcollege.net>
2021-05-09 13:49:04 -0400

    Auto stash before rebase of "Patrick"

---
+++ WorkPunch/WorkPunch/Model/Chargeable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPunch
+{
+    abstract class Chargeable
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public long price { get; set; }
+        public virtual long CalculateTotalCost()
+        {
+            return price;
+        }
+    }
+}
---
+++ WorkPunch/WorkPunch/Model/Comission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPunch.Model
+{
+    class Comission : Chargeable
+    {
+        public double commissionPercent { get; set; }
+        public Comission(int id,string name, long price, double commissionPercent)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.commissionPercent = commissionPercent;
+        }
+        public override long CalculateTotalCost()
+        {
+            return price * (long)(commissionPercent / 100);
+        }
+    }
+}
